ba4a4af4cc6a5840492fade066466f800153c02d
RafaelLibrero <librerorafael@outlook.com>
2025-04-21 18:51:58 +0200

    Base design and default language will be spanish

--- app/src/main/java/com/boxbox/app/ui/home/adapter/TopicViewHolder.kt
+++ app/src/main/java/com/boxbox/app/ui/home/adapter/TopicViewHolder.kt
@@ -2,14 +2,20 @@ package com.boxbox.app.ui.home.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.boxbox.app.R
 import com.boxbox.app.databinding.ItemTopicBinding
 import com.boxbox.app.domain.model.VTopic
 
 class TopicViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
-    private val binding =  ItemTopicBinding.bind(view)
+    private val binding = ItemTopicBinding.bind(view)
 
     fun render(topic: VTopic) {
         binding.tvTitle.text = topic.title
+        binding.tvDescription.text = topic.description
+        binding.tvConversations.text =
+            binding.tvConversations.context.getString(R.string.conversations, topic.conversations)
+        binding.tvPosts.text =
+            binding.tvPosts.context.getString(R.string.posts, topic.posts)
     }
 }
