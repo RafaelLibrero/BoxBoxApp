1371e86fb2d40587f79c4e74ba5e4840c0098405
RafaelLibrero <librerorafael@outlook.com>
2025-05-08 17:36:30 +0200

    Colors for top 3 and other adjustments for drivers and teams

--- app/src/main/java/com/boxbox/app/ui/season/tab/drivers/DriversViewHolder.kt
+++ app/src/main/java/com/boxbox/app/ui/season/tab/drivers/DriversViewHolder.kt
@@ -1,7 +1,9 @@
 package com.boxbox.app.ui.season.tab.drivers
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.boxbox.app.R
 import com.boxbox.app.databinding.ItemDriverBinding
 import com.boxbox.app.domain.model.Driver
 import com.squareup.picasso.Picasso
@@ -11,11 +13,20 @@ class DriversViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemDriverBinding.bind(view)
 
     fun render(driver: Driver, position: Int) {
-        binding.tvPosition.text = position.toString()
-        Picasso.get().load(driver.imagen).into(binding.ivDriver)
-        binding.tvName.text = driver.driverName
-        binding.tvCarNumber.text = driver.carNumber.toString()
-        Picasso.get().load(driver.flag).into(binding.ivFlag)
-        binding.tvPoints.text = driver.points.toString()
+        with (binding) {
+            tvPosition.text = position.toString()
+            Picasso.get().load(driver.imagen).into(ivDriver)
+            tvName.text = driver.driverName
+            tvCarNumber.text = driver.carNumber.toString()
+            Picasso.get().load(driver.flag).into(ivFlag)
+            tvPoints.text = driver.points.toString()
+
+            when (position) {
+                1 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.gold))
+                2 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.silver))
+                3 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.bronze))
+                else -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.transparent))
+            }
+        }
     }
 }
--- app/src/main/java/com/boxbox/app/ui/season/tab/teams/TeamsViewHolder.kt
+++ app/src/main/java/com/boxbox/app/ui/season/tab/teams/TeamsViewHolder.kt
@@ -1,7 +1,9 @@
 package com.boxbox.app.ui.season.tab.teams
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.boxbox.app.R
 import com.boxbox.app.databinding.ItemTeamBinding
 import com.boxbox.app.domain.model.Team
 import com.squareup.picasso.Picasso
@@ -11,9 +13,18 @@ class TeamsViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemTeamBinding.bind(view)
 
     fun render(team: Team, position: Int) {
-        binding.tvPosition.text = position.toString()
-        Picasso.get().load(team.logo).into(binding.ivLogo)
-        binding.tvName.text = team.teamName
-        binding.tvPoints.text = team.points.toString()
+        with (binding) {
+            tvPosition.text = position.toString()
+            Picasso.get().load(team.logo).into(ivLogo)
+            tvName.text = team.teamName
+            tvPoints.text = team.points.toString()
+
+            when (position) {
+                1 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.gold))
+                2 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.silver))
+                3 -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.bronze))
+                else -> tvPosition.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.transparent))
+            }
+        }
     }
 }
