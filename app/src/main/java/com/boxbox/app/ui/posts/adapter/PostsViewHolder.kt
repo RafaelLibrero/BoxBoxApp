e86e1d104b04ab58db59720a369f6824e0e3fb8e
RafaelLibrero <librerorafael@outlook.com>
2025-06-19 19:47:58 +0200

    Improvements posts design layout

--- app/src/main/java/com/boxbox/app/ui/posts/adapter/PostsViewHolder.kt
+++ app/src/main/java/com/boxbox/app/ui/posts/adapter/PostsViewHolder.kt
@@ -4,6 +4,8 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import coil3.load
 import coil3.request.crossfade
+import coil3.request.transformations
+import coil3.transform.CircleCropTransformation
 import com.boxbox.app.databinding.ItemPostBinding
 import com.boxbox.app.domain.model.PostWithUser
 import com.boxbox.app.utils.DateFormatter
@@ -19,7 +21,10 @@ class PostsViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         with (binding) {
             tvUsername.text = user!!.userName
-            ivAvatar.load(user.profilePicture) { crossfade(true) }
+            ivAvatar.load(user.profilePicture) {
+                crossfade(true)
+                transformations(CircleCropTransformation())
+            }
             tvText.text = post.text
             tvCreatedAt.text = DateFormatter.formatToMinutes(post.createdAt)
         }
